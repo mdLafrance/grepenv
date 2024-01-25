72afa1474367692ea898443d8c531cd5cb82848e
mdLafrance <maxlafrance97@gmail.com>
2024-01-25 18:34:54 -0500

    Adds invoke, and a task file for building

---
+++ tasks.py
@@ -0,0 +1,22 @@
+"""Command line runnable task definitions for use with pyinvoke.
+"""
+from invoke import task
+
+@task
+def lint(c):
+    c.run("poetry run black ./src")
+
+
+@task
+def test(c):
+    c.run("poetry run coverage run -m pytest tests/")
+    c.run("poetry run coverage report --omit=\"tests/*\" --fail-under=80")
+
+
+@task
+def build(c):
+    c.run("poetry build")
+
+@task
+def publish(c):
+    c.run("poetry publish")
